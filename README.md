f54aead7973577101d1a8bbcb368bc4d50c4d890
Matheus <93588969+roqueds@users.noreply.github.com>
2022-04-01 00:15:33 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+# basic_calculator
+
+Description. 
+The package basic_calculator is used to:
+	- Addition operation
+	- Subtraction operation
+	- Multiplication operation
+	- Division operation
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install basic_calculator
+
+```bash
+pip install basic_calculator
+```
+
+## Usage
+
+```python
+from basic_calculator import operation
+operation.addition(x,y)
+```
+
+## Author
+Matheus Roque
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="basic_calculator",
+    version="0.0.1",
+    author="Matheus Roque",
+    author_email="mroque@outlook.com.br",
+    description="The package is used to basic mathematics operations",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/roqueds/basic_calculator",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
